7e212079b755befdb5a6060d089a94fb78f63cd4
Diana Yepseu <dianayepseu04@gmail.com>
2026-03-07 19:41:01 -0500

    #14
    Footer

---
+++ Site CookieBook/general/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+    <div className="wrap">
+      <div className="container footer-inner">
+        <div>
+          <div className="footer-title">CookieBook</div>
+          <div className="muted">Fait maison • Sur commande • 2 boutiques seulement au Québec</div>
+        </div>
+        <div className="muted">
+          Horaires: Lun–Sam 10h–18h • Délais: 24–48h selon la commande
+        </div>
+      </div>
+      </div>
+    </footer>
+  );
+}
+
